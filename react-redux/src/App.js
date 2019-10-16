274e39a354424160117b34944551686e9e98f126
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-16 13:50:44 -0300

    Reducer
    Criando um reducer

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import Video from "./components/Video";
+import Sidebar from "./components/Sidebar";
+
+import Store from './store';
 
 function App() {
   return (
     <div className="App">
-      
+      <Provider store = {Store}>
+        <Video/> 
+        <Sidebar />
+      </Provider>
     </div>
   );
 }
--- react-redux/src/components/Sidebar/index.js
+++ react-redux/src/components/Sidebar/index.js
@@ -1,35 +1,21 @@
 import React, { Component } from 'react'
 
-export default class Sidebar extends Component {
-    state = {
-        modules: [
-            {
-                id: 1, title: "iniciando com React", lessons: [
-                    { id: 1, title: 'Primeira aula' },
-                    { id: 2, title: 'Segunda aula' },
-                ],
-                id: 2, title: 'Aprendendo Redux', lessons:[
-                    { id: 3, title: 'Terceira aula' },
-                    { id: 4, title: 'Quarta aula' },
-                ]
-            }
-        ]
-    }
-    render() {
-        const {module} = this.state;
-        return (
-            <aside>
-                {modules.map(module => (
-                    <div key={module.id}>
-                        <strong>{module.tile}</strong>
-                        <ul>
-                            {module.lessons.map(lesson => (
-                                <li key={lesson.id}>{lesson.title}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </aside>
-        )
-    }
-}
+import {connect} from 'react-redux';
+
+const Sidebar = ({modules}) => (
+        <aside>
+        {modules.map(module => (
+               <div key={module.id}>
+                   <strong>{module.tile}</strong>
+                   <ul>
+                       {module.lessons.map(lesson => (
+                           <li key={lesson.id}>{lesson.title}</li>
+                       ))}
+                   </ul>
+               </div>
+           ))}
+    </aside>
+    );
+    //crie uma propriedade(modules) que recebe o estado inteiro do redux
+    export default connect(state => ({modules: state}))(Sidebar);
+
